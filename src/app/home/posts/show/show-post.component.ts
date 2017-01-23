70e818ce348978a3b821e02213116e97b94bba11
jomarbarnobal <jomar.barnobal@outlook.com>
2017-01-24 06:11:38 +0800

    add css for social share button

--- src/app/home/posts/show/show-post.component.ts
+++ src/app/home/posts/show/show-post.component.ts
@@ -13,6 +13,7 @@ export class SinglePostComponent implements OnInit{
   post = []
   comments = []
   facebookButton;
+  twitterButton;
   description = [];
 
   id: number;
@@ -35,9 +36,15 @@ export class SinglePostComponent implements OnInit{
 
     this.facebookButton = new ShareButton(
         ShareProvider.FACEBOOK,              //choose the button from ShareProvider
-        "<img src='../../assets/img/custom/single-twitter.svg'> facebook",    //set button template
+        "<span><i class='fa fa-facebook'></i></span>", //set button template
         'facebook'                           //set button classes
       );
+
+    this.twitterButton = new ShareButton(
+        ShareProvider.TWITTER,              //choose the button from ShareProvider
+        "<span><i class='fa fa-twitter'></i></span>", //set button template
+        'twitter'                           //set button classes
+      );   
   }
 }
 
