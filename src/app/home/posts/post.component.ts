7e2837dd2268026af4db39a4906dc72a66a99847
jomarbarnobal <jomar.barnobal@outlook.com>
2017-01-24 08:40:27 +0800

    add social share button in home page

--- src/app/home/posts/post.component.ts
+++ src/app/home/posts/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {ShareButton, ShareProvider} from "ng2-sharebuttons";
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from '../../services';
 import { Router } from '@angular/router';
@@ -11,7 +12,9 @@ import { Observable } from 'rxjs/Rx'
 })
 
 export class PostsComponent implements OnInit {
-
+  facebookButton;
+  twitterButton;
+  
   posts: Observable<any[]>;
 
   constructor(
@@ -20,6 +23,19 @@ export class PostsComponent implements OnInit {
   
   ngOnInit(){
     this.getPosts();
+
+    this.facebookButton = new ShareButton(
+        ShareProvider.FACEBOOK,              //choose the button from ShareProvider
+        "<span><i class='fa fa-facebook'></i></span>", //set button template
+        'facebook'                           //set button classes
+      );
+
+    this.twitterButton = new ShareButton(
+        ShareProvider.TWITTER,              //choose the button from ShareProvider
+        "<span><i class='fa fa-twitter'></i></span>", //set button template
+        'twitter'                           //set button classes
+      );   
+
   }
   
   getPosts(){
